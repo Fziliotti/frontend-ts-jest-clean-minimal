e8716f2ab9324c2ec06c808dd2d07a10847c1c5a
Fabricio Ziliotti <ffz.ziliotti@gmail.com>
2020-07-16 00:40:29 -0300

    Feat: add react webpack and sass version

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,16 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: [
-    '<rootDir>/src/**/*.{ts,tsx}'
+    '<rootDir>/src/**/*.{ts,tsx}',
+    '!**/*.d.ts'
   ],
   coverageDirectory: 'coverage',
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
   transform: {
-    '.+\\.ts$': 'ts-jest'
+    '.+\\.(ts|tsx)$': 'ts-jest'
   },
   moduleNameMapper: {
-    '@/(.*)': '<rootDir>/src/$1'
+    '@/(.*)': '<rootDir>/src/$1',
+    '\\.scss$': 'identity-obj-proxy' // toda vez que o jest encontra o scss, ela usa o test double  para gerar o dummy
   }
 }
